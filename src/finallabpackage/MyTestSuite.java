47eb8b18698e192e549c5b9128ba81d2a200f68e
MyPc <MyPc@172.16.108.179>
2016-06-19 13:41:46 +0500

    Commit2

--- src/finallabpackage/MyTestSuite.java
+++ src/finallabpackage/MyTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Addclasstest.class, Multiplyclasstest.class, Subtractclasstest.class })
+@SuiteClasses({ Addclasstest.class, Multiplyclasstest.class, Subtracttest.class })
 public class MyTestSuite {
 
 }
--- src/finallabpackage/Subtracttest.java
+++ src/finallabpackage/Subtracttest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class Subtractclasstest {
+public class Subtracttest {
 
 	@Test
 	public void test() {
